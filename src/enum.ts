fb044531401f023c91309890d8738010c66906d5
cocoaaa3 <dona69.914aaa@gmail.com>
2022-10-24 16:07:15 +0900

    create intersection.ts, enum.ts and function.ts

---
+++ src/enum.ts
@@ -0,0 +1,18 @@
+export{} ; 
+
+enum Months {
+  January,
+  February,
+  March, 
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  Novemver,
+  December
+}
+
+console.log(Months.January);
