257f21be65650c88f6ca17db31971912a6c6fec4
cocoaaa3 <dona69.914aaa@gmail.com>
2022-10-25 15:30:26 +0900

    create two files

---
+++ src/type-compatibility.ts
@@ -0,0 +1,21 @@
+export {}; 
+
+let fooCompatible: any;
+let barCompatible: string = 'TypeScript';
+
+console.log(typeof fooCompatible);
+
+fooCompatible = barCompatible;
+
+console.log(typeof fooCompatible);
+
+let fooIncompatible: string;
+let barIncompatible: number = 1;
+
+// fooIncompatible = barInCompatible
+
+let fooString: string;
+let barString: string = 'string';
+
+fooString = barString;
+
