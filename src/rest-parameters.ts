d22e59186da18f09755d50cc9d19a20475e49def
cocoaaa3 <dona69.914aaa@gmail.com>
2022-10-25 10:14:30 +0900

    create default-parameters.ts, rest-parameters.ts, overloads.ts

---
+++ src/rest-parameters.ts
@@ -0,0 +1,14 @@
+export {}; 
+
+const reducer = (accumlator: number, currentValue: number) => {
+  console.log({ accumlator, currentValue})
+  return accumlator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (...values: number[]):number => {
+  return values.reduce(reducer)
+};
+
+console.log(sum(1, 2, 3, 4, 5));
+
+// [1, 2, 3, 4].reduce(reducer);
